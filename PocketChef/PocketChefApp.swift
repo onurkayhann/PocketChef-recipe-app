c54d9581b1455e1588984d75c42ac5991fecdb1f
Onur <onur.kayhan90@gmail.com>
2025-04-30 11:26:57 +0200

    Initial Commit

---
+++ PocketChef/PocketChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketChefApp.swift
+//  PocketChef
+//
+//  Created by Onur Kayhan on 2025-04-30.
+//
+
+import SwiftUI
+
+@main
+struct PocketChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
