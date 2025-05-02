b16a470c85e8cb1346fd157ec7548375a43e109d
Onur <onur.kayhan90@gmail.com>
2025-05-02 19:15:35 +0200

    Add temporarily design to LoginView and RegisterView

--- PocketChef/Views/Unauthenticated/LoginView.swift
+++ PocketChef/Views/Unauthenticated/LoginView.swift
@@ -1,8 +1,49 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text("Login")
+        VStack {
+            Image("PocketChef-logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+                        
+            VStack(spacing: 20) {
+                Text("Login")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundStyle(.white)
+
+                TextField("Email", text: $email)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .frame(width: 250)
+                    .cornerRadius(8)
+                    .keyboardType(.emailAddress)
+                    .autocapitalization(.none)
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .frame(width: 250)
+                    .cornerRadius(8)
+                
+                Button("Login") {
+                    // login
+                }
+                .frame(width: 200)
+                .padding()
+                .background(Color("DarkerBlue"))
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            }
+            .frame(width: 300, height: 500)
+            .background(Color("Blue"))
+            .cornerRadius(16)
+        }
     }
 }
 
--- PocketChef/Views/Unauthenticated/RegisterView.swift
+++ PocketChef/Views/Unauthenticated/RegisterView.swift
@@ -1,10 +1,59 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var confirmPassword: String = ""
+    
     var body: some View {
-        Text("RegisterView")
+        VStack {
+            Image("PocketChef-logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+                        
+            VStack(spacing: 20) {
+                Text("Register")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundStyle(.white)
+
+                TextField("Email", text: $email)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .frame(width: 250)
+                    .cornerRadius(8)
+                    .keyboardType(.emailAddress)
+                    .autocapitalization(.none)
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .frame(width: 250)
+                    .cornerRadius(8)
+                
+                SecureField("Confirm Password", text: $password)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .frame(width: 250)
+                    .cornerRadius(8)
+                
+                Button("Register") {
+                    // register
+                }
+                .frame(width: 200)
+                .padding()
+                .background(Color("DarkerBlue"))
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            }
+            .frame(width: 300, height: 500)
+            .background(Color("Blue"))
+            .cornerRadius(16)
+        }
     }
 }
+
 
 #Preview {
     RegisterView()
