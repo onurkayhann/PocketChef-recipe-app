e6e2a867446e471468487e625d82577a1862be4f
Onur <onur.kayhan90@gmail.com>
2025-05-04 21:34:19 +0200

    Create Firebase project, connect it to app, install necessary packages, and import plist to the app

--- PocketChef/PocketChefApp.swift
+++ PocketChef/PocketChefApp.swift
@@ -1,14 +1,18 @@
-//
-//  PocketChefApp.swift
-//  PocketChef
-//
-//  Created by Onur Kayhan on 2025-04-30.
-//
-
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct PocketChefApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
