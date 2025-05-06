8ac594eb8fed16ebb3ca6587b5552fe60fbce9a1
Onur <onur.kayhan90@gmail.com>
2025-05-06 12:08:31 +0200

    WIP: Add loginUser and registerUser func in DbConnection, work in progress

--- PocketChef/ViewModel/DbConnection.swift
+++ PocketChef/ViewModel/DbConnection.swift
@@ -8,6 +8,52 @@ class DbConnection {
     let COLLECTION_RECIPES = "recipes"
     var recipes: [Recipe] = []
     
+    let COLLECTION_USER_DATA = "user_data"
+    @Published var currentUser: User?
+    @Published var currentUserData: UserData?
+    
+    func registerUser(name: String, email: String, password: String, confirmPassword: String) {
+        guard password == confirmPassword else {
+            print("Error: Passwords do not match!")
+            return
+        }
+        
+        auth.createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let authResult = authResult else { return }
+            
+            let newUserData = UserData(name: name, favoriteRecipes: [])
+            
+            do {
+                try self.db.collection(self.COLLECTION_USER_DATA).document(authResult.user.uid).setData(from: newUserData)
+            } catch let error {
+                print("Failed to create userData: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func loginUser(email: String, password: String) {
+        auth.signIn(withEmail: email, password: password)
+    }
+    
+    init() {
+        auth.addStateDidChangeListener { auth, user in
+            
+            if let user = user {
+                // Användaren har loggat in
+                self.currentUser = user
+                self.startListeningToDb()
+            } else {
+                // Användaren har loggat ut
+                self.currentUser = nil
+            }
+        }
+    }
+    
     func deleteRecipe(id: String) {
         let recipeToDelete = recipes.first { $0.id == id }
         
