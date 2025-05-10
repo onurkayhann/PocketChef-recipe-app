82d27f8193e7be92b66b2f28b606b1fcd9366b73
Onur <onur.kayhan90@gmail.com>
2025-05-10 11:00:56 +0200

    Add: Add another ShadowButton for Logout, and move logout from HomeView to SettingsView

--- PocketChef/Views/Authenticated/SettingsView.swift
+++ PocketChef/Views/Authenticated/SettingsView.swift
@@ -1,18 +1,25 @@
-//
-//  SettingsView.swift
-//  PocketChef
-//
-//  Created by Onur Kayhan on 2025-05-09.
-//
-
 import SwiftUI
 
 struct SettingsView: View {
+    @EnvironmentObject var db: DbConnection
+    
     var body: some View {
-        Text("This is SettingsView")
+        VStack {
+            Text("Hello \(db.currentUserData?.name ?? "")! Here you can edit your settings")
+            Spacer()
+            LogoutButton(text: "Logout", action: {
+                db.signOut()
+            })
+            .frame(width: 250)
+            .padding(.bottom, 40)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color("background"))
+        .ignoresSafeArea()
     }
 }
 
 #Preview {
     SettingsView()
+        .environmentObject(DbConnection())
 }
